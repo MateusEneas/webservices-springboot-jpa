ec91c9818eeed67a19196755751ecb62b2b061e4
Mateus Eneas <mateus.eneas98@gmail.com>
2025-02-01 12:26:03 -0300

    Adicionando exceção no findById

--- src/main/java/com/eneasdev/webservices/entities/Order.java
+++ src/main/java/com/eneasdev/webservices/entities/Order.java
@@ -18,7 +18,7 @@ public class Order implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy'T'HH:mm:ss'Z'", timezone = "GMT")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy'T'HH:mm:ss'Z'", timezone = "GMT-3")
     private Instant moment;
 
     private Integer orderStatus;
--- src/main/java/com/eneasdev/webservices/services/UserService.java
+++ src/main/java/com/eneasdev/webservices/services/UserService.java
@@ -2,6 +2,7 @@ package com.eneasdev.webservices.services;
 
 import com.eneasdev.webservices.entities.User;
 import com.eneasdev.webservices.repositories.UserRepository;
+import com.eneasdev.webservices.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user) {
