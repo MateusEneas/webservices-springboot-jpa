271d092d13f0f10765a4a03e8186fab3ffe2697c
Mateus Eneas <mateus.eneas98@gmail.com>
2025-02-01 12:54:08 -0300

    Adicionando exceção no update

--- src/main/java/com/eneasdev/webservices/services/UserService.java
+++ src/main/java/com/eneasdev/webservices/services/UserService.java
@@ -4,6 +4,7 @@ import com.eneasdev.webservices.entities.User;
 import com.eneasdev.webservices.repositories.UserRepository;
 import com.eneasdev.webservices.services.exceptions.DatabaseException;
 import com.eneasdev.webservices.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -43,9 +44,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, obj);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
